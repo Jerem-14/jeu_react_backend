2139117a0c51c6faeb2104dcf0d68f398afb48e7
Jerem-14 <jeremy.aubry69@gmail.com>
2025-01-20 21:40:00 +0100

    maj bdd.js

--- src/bdd.js
+++ src/bdd.js
@@ -10,26 +10,10 @@ const isDevelopment = process.env.NODE_ENV !== 'PROD';
  * Connexion à la base de données
  */
 export const sequelize = new Sequelize({
-    dialect: MySqlDialect,
-    database: process.env.DB_NAME || "database_name",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT) || 3306,
-    ...(isDevelopment ? {} : {
-        ssl: true,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
-    }),
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    connectTimeout: 60000
-});
+	dialect: MySqlDialect,
+	database: process.env.DB_NAME || 'database_name',
+	user: process.env.DB_USER || 'root',
+	password: process.env.DB_PASSWORD || '',
+	host: process.env.DB_HOST || 'localhost',
+	port: parseInt(process.env.DB_PORT) || 3306
+})

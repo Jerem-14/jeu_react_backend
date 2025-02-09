13c644a28e78521b68f25e88dd4fc987aeb92c3f
Jerem-14 <jeremy.aubry69@gmail.com>
2025-02-09 20:56:12 +0100

    fix env var name

--- src/models/media.js
+++ src/models/media.js
@@ -15,7 +15,7 @@ const Media = sequelize.define("media", {
     url: {
         type: DataTypes.STRING,
         get() {
-            return `${BASE_URL}${this.getDataValue('url')}`;
+            return `${VITE_API_URL}${this.getDataValue('url')}`;
         }
     },
     filename: {
